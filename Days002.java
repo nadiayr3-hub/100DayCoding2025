64aa36299ad2c553a3f05f515f72c6324f11c59b
nadiayr3-hub <nadiayr3@gmail.com>
2025-09-09 19:39:48 +0800

    PrintlPrintPrintf

---
+++ Days002.java
@@ -0,0 +1,10 @@
+public class day002
+{
+    public static void main(String[] args) {
+    System.out.println("nama kamu siapa:");
+    System.out.print("nama aku nadia, ");
+    System.out.printf("umur aku %s tahun", 18);
+
+        
+    }
+}
