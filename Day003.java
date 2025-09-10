3f896b8a69674d2599a4a0bc14dcb8b230289727
nadiayr3-hub <nadiayr3@gmail.com>
2025-09-10 22:52:18 +0800

    Komentar

---
+++ Day003.java
@@ -0,0 +1,11 @@
+public class day003
+{
+    public static void main(String[] args) {
+    // komentar saya
+    System.out.println("komentar saya");
+    // Komentar itu catatan untuk kode
+
+     System.out.printf("nama saya nadia, %d tahun", 18);
+     
+    }
+}
