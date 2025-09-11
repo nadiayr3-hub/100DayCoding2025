d3847c1d0f598bd0f29bf670dafef58fa1505e6b
nadiayr3-hub <nadiayr3@gmail.com>
2025-09-11 22:38:08 +0800

    penggunaanESPACE

---
+++ Days004.java
@@ -0,0 +1,9 @@
+public class day004
+{
+    public static void main(String[] args) {
+    String nama = "Nadia";
+        int umur = 19;
+
+        System.out.println("Nama: " + nama + "\nUmur: " + umur);
+    }
+}
