999bc0ff615c18e38e90252a43e4573a9525070a
nadiayr3-hub <nadiayr3@gmail.com>
2025-09-12 22:37:05 +0800

    Tipe data numerik

---
+++ Days005.java
@@ -0,0 +1,14 @@
+public class days004
+{
+    public static void main(String[] args) {
+              short tinggiBadan = 165; 
+        byte umur = 18;
+        float beratBadan = 50.3f;
+        long nomorHP = 62895800599727l;
+
+        System.out.println("Tinggi Badan: " + tinggiBadan + " cm");
+        System.out.println("Umur: " + umur + " tahun");
+        System.out.println("Berat Badan: " + beratBadan + " kg");
+        System.out.println("Nomor HP: " + nomorHP);
+    }
+}
