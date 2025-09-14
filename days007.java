192acea8a591ebbed286ca13b714d6b46f204559
nadiayr3-hub <nadiayr3@gmail.com>
2025-09-14 11:35:00 +0800

    CharBoolean

---
+++ days007.java
@@ -0,0 +1,10 @@
+public class Program
+{
+    public static void main(String[] args) {
+        char jenisKelamin = 'P'; 
+        boolean sudahMandi = true;
+
+        System.out.println("Jenis Kelamin: " + jenisKelamin);
+        System.out.println("sudah mandi: " + sudahMandi);
+    }
+}
