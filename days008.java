422ae37bee81f1d9537284ea412d9568a0e5efce
nadiayr3-hub <nadiayr3@gmail.com>
2025-09-15 21:24:53 +0800

    string

---
+++ days008.java
@@ -0,0 +1,11 @@
+public class day008
+{
+    public static void main(String[] args) {
+        String nama = "nadia";
+        String prodi = "sistem informasi";
+
+        System.out.println("hai namaku " 
+        + nama + ", dari program studi " + prodi);
+        
+    }
+}
