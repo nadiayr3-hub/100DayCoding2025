15f6b09013dd79a8cc2ee33a89e7e8df58cc2c96
nadiayr3-hub <nadiayr3@gmail.com>
2025-09-16 22:10:43 +0800

    deklarasiInisialisasiUpdateVariabel

---
+++ days009.java
@@ -0,0 +1,29 @@
+public class days008
+{
+    public static void main(String[] args) {
+         int umur;
+        String nama;
+        double tinggi;
+        String status;
+
+        umur = 17;
+        nama = "Nadia";
+        tinggi = 163.5;
+        status = "belum jadi istri mingyu";
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("tinggi: " + tinggi);
+        System.out.println("status: " + status);
+
+        umur = 18;
+        nama = "Nadia";
+        tinggi = 165.5;
+        status = "sudah jadi istri mingyu";
+
+        System.out.println("nama: " + nama);
+        System.out.println("umur: " + umur);
+        System.out.println("tinggi :" + tinggi);
+        System.out.println("status: " + status);
+    }
+}
