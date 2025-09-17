efa2bdb6b879fcc0694cce0edbe825eefefdaad9
nadiayr3-hub <nadiayr3@gmail.com>
2025-09-17 22:59:16 +0800

    mingyu

---
+++ Days10.java
@@ -0,0 +1,10 @@
+public class days10
+{
+    public static void main(String[] args) {
+        
+        final String tempat_tinggal = "polewali mandar";
+
+        System.out.println("tempat tinggal: " + tempat_tinggal);
+
+    }
+}
